5792ee1cfce7a765a5e9642f9be244a4437f378e
devbxtzz <batztonnie@gmail.com>
2022-09-23 18:28:39 +0300

    ICO Project Updates

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x52B573aacBbd89611DD0C66d7A954388A4Ec193b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
